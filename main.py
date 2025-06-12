91f56db154f8e123efdc6e31ff28d6550fce02b0
debjit-1004 <debjitkundu11@gmail.com>
2025-06-12 19:09:40 +0530

    added api call ...now it can be delpoyed yayyyy...

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+#from routes import summarise_route
+from fastapi.middleware.cors import CORSMiddleware
+from app import fetch_github_profile
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+
+@app.get("/api/readme")
+async def readme(repo: str):
+    try:
+        # not await fetch_github..as it is not a async fn 
+        data =  fetch_github_profile(repo)
+        return JSONResponse(content=data)
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
+    
+
+#app.include_router(summarise_route.router)
